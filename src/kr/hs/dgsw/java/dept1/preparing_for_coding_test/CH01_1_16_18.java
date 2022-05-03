24c83f9f1abe4dd8784413fcd618cd79e46f35fb
nrbns357 <gusgh357@gmail.com>
2022-05-03 21:01:08 +0900

    ADD: CH01_2_19_22

--- src/kr/hs/dgsw/java/dept1/preparing_for_coding_test/CH01_1_16_18.java
+++ src/kr/hs/dgsw/java/dept1/preparing_for_coding_test/CH01_1_16_18.java
@@ -1,6 +1,6 @@
 package kr.hs.dgsw.java.dept1.preparing_for_coding_test;
 
-public class Chapter01_1_page_From_16_to_18 {
+public class CH01_1_16_18 {
 //시간 복잡도는 주어진 문제를 해결하기 위한 연산 횟수를 말합니다.
 	//일반적으로 수행 시간은 1억 번의 연산을 1초의 시간으로 간주하여 예측합니다.
 	
@@ -19,5 +19,4 @@ public class Chapter01_1_page_From_16_to_18 {
 	
 	//코딩 테스트에서는 어떤 시간 복잡도 유형을 사용해야 할까?
 	//(O(n))을 기준으로 수행시간을 계산하는 것이 좋습니다.
-	
 }
---
+++ src/kr/hs/dgsw/java/dept1/preparing_for_coding_test/CH01_2_19_22.java
@@ -0,0 +1,20 @@
+package kr.hs.dgsw.java.dept1.preparing_for_coding_test;
+
+import java.util.Scanner;
+
+public class CH01_2_19_22 {
+    public static void main(String[] args) {
+        // 문제(연습) 000번 - 수정렬하기 시간제한 2초 백준 2750번
+        // N개의 수가 주어졌을 때 이름 오름차순 정렬하는 프로그램을 작성하시오.
+        
+        
+        Scanner sc = new Scanner(System.in);
+        int Number1, ;
+        Number1 = sc.nextInt();
+
+        for(int i = 0; i < Number1; i++){
+            System.out.println(Number1 - i);      
+        }
+
+    }
+}
